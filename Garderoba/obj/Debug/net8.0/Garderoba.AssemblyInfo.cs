2e6e89bb0df75728f0d038af43516c027508b852
lkocis <168871650+lkocis@users.noreply.github.com>
2025-07-21 20:18:39 +0200

    Some changes on assembly info.

--- Garderoba/obj/Debug/net8.0/Garderoba.AssemblyInfo.cs
+++ Garderoba/obj/Debug/net8.0/Garderoba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garderoba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb47ae0abb08385eadd1fff86194fa9f3aaefc43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e0b7f763e50156b4eb64232818eb4aa8286599")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garderoba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garderoba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
